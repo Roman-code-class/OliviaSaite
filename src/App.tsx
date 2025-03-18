721aff2b88e4bfed580bbbdfda1864706e7d3db9
Roman <mac@Mac-my-Roman.local>
2025-03-18 21:45:25 +0300

    проьлема с шрифтами 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import "./App.css";
-
 function App() {
   return (
     <div className="p-10 bg-gray-100 min-h-screen">
-      <h1 className="text-4xl font-bold font-montserrat text-blue-600">
+      {/* <h1 className="text-4xl font-bold font-[var(--font-montserrat)] text-blue-600">
         Привет, мир!
       </h1>
-      <p className="text-lg font-inter text-gray-700">
+      <p className="text-lg font-[var(--font-inter)] text-gray-700">
         Это текст с шрифтом Inter.
       </p>
-      <p className="text-lg font-plateia text-gray-900">
+      <p className="text-lg font-[var(--font-plateia)] text-gray-900">
         А это текст с шрифтом Plateia.
-      </p>
+      </p> */}
     </div>
   );
 }
