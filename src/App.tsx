d5884c429959f820692fcc7562fefcae63cfc6ae
Roman <mac@Mac-my-Roman.local>
2025-03-18 20:17:10 +0300

    first commit1

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold"> Привет мир</h1>
+      <h1 className="text-3xl font-bold underline"> Привет мир</h1>
     </div>
   );
 }
